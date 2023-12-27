49ed0f7b163d180c49e5323858b647b1af6f5b69
Felipe Fraxe <felipe_fraxe@hotmail.com>
2023-12-27 08:41:06 -0400

    Ajuste na f

--- questao_f.py
+++ questao_f.py
@@ -22,32 +22,6 @@ WITH RECURSIVE category_tree AS (
 FROM category_tree;
 """
 
-"""
-
-    SELECT category_id, name, parent_id
-    FROM category ct JOIN productscategories ON category_id = category_id_fk JOIN products ON product_id = product_id_fk
-    WHERE category_id
-    GROUP BY category_id
-    ORDER BY AVG(CASE WHEN helpful > 0 THEN rating ELSE NULL END) DESC, AVG(CASE WHEN helpful > 0 THEN helpful ELSE NULL END) DESC
-
-    UNION
-
-    SELECT c.category_id, c.name, c.parent_id
-    FROM category c
-    JOIN category_tree ct ON c.category_id = ct.parent_id
-) SELECT *
-FROM category_tree;
-"""
-"""
-SELECT category_id_fk, AVG(CASE WHEN helpful > 0 THEN rating ELSE NULL END) AS avg_rating, AVG(CASE WHEN helpful > 0 THEN helpful ELSE NULL END) AS avg_helpful
-FROM (
-    products JOIN productscategories ON product_id = product_id_fk
-) JOIN reviews ON product_id = reviews.product_id_fk
-GROUP BY category_id_fk
-ORDER BY avg_rating DESC, avg_helpful DESC
-HAVING AVG(CASE WHEN helpful > 0 THEN rating ELSE NULL END) IS NOT NULL
-LIMIT 5;
-"""
 resuls = exec_query(query=[sql_question_f], is_select=True)
 
 if len(resuls) == 0:
